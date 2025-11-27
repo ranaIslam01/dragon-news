4a45c544e3d2c8b494412e1441128b9055840e18
Rana Islam <dev.rana.cse@gmail.com>
2025-11-27 20:56:21 +0600

    Firebase

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,17 +1,19 @@
-// src/Firebase/firebase.config.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId
+  apiKey: "AIzaSyDN9bPevjQMLTftmj3_IHmeTVaMktevYAM",
+  authDomain: "dragon-news-2-1d8e0.firebaseapp.com",
+  projectId: "dragon-news-2-1d8e0",
+  storageBucket: "dragon-news-2-1d8e0.firebasestorage.app",
+  messagingSenderId: "201733596962",
+  appId: "1:201733596962:web:8e92e50bae91844823677b"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 export default app;
